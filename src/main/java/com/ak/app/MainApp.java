7c300f9924fa231e9344e101a14bb4467bf3d86a
Alexander Kobelev <ak.mail.ru@gmail.com>
2024-01-22 21:17:41 +0300

    Add package-info.java

--- src/main/java/com/ak/app/MainApp.java
+++ src/main/java/com/ak/app/MainApp.java
@@ -1,13 +1,12 @@
 package com.ak.app;
 
-import javax.annotation.Nonnull;
 import java.util.logging.Logger;
 
 public class MainApp {
   private MainApp() {
   }
 
-  public static void main(@Nonnull String[] args) {
-    Logger.getLogger(MainApp.class.getName()).info(() -> "Hello word 2023.12.21!");
+  public static void main(String[] args) {
+    Logger.getLogger(MainApp.class.getName()).info(() -> "Hello word 2024.01.22!");
   }
 }
