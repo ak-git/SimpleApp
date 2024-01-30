85f459eb44a51308abcee27c36a2e2cee736d9a0
Alexander Kobelev <ak.mail.ru@gmail.com>
2024-01-30 21:24:01 +0300

    Java 21

--- src/main/java/com/ak/app/MainApp.java
+++ src/main/java/com/ak/app/MainApp.java
@@ -7,6 +7,6 @@ public class MainApp {
   }
 
   public static void main(String[] args) {
-    Logger.getLogger(MainApp.class.getName()).info(() -> "Hello word 2024.01.22!");
+    Logger.getLogger(MainApp.class.getName()).info(() -> "Hello word 2024.01.30!");
   }
 }
