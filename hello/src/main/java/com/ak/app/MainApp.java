35d36d72f57492df16298e4e8b52159975536d6e
Alexander Kobelev <ak.mail.ru@gmail.com>
2024-07-26 12:01:57 +0300

    Update Gradle to 8.9

--- hello/src/main/java/com/ak/app/MainApp.java
+++ hello/src/main/java/com/ak/app/MainApp.java
@@ -7,6 +7,6 @@ public class MainApp {
   }
 
   public static void main(String[] args) {
-    Logger.getLogger(MainApp.class.getName()).info(() -> "Hello word 2024.06.24!");
+    Logger.getLogger(MainApp.class.getName()).info(() -> "Hello word 2024.07.26!");
   }
 }
