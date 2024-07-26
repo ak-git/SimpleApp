0ac4143082435d90fe655b3bbab47c8eee908fb4
Alexander Kobelev <ak.mail.ru@gmail.com>
2024-07-26 11:57:46 +0300

    Fix constants

--- hello/src/main/java/com/ak/app/MainApp.java
+++ hello/src/main/java/com/ak/app/MainApp.java
@@ -9,6 +9,7 @@ import org.jfugue.rhythm.Rhythm;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.logging.Logger;
 import java.util.stream.IntStream;
 
@@ -19,12 +20,13 @@ public class MainApp {
   public static void main(String[] args) throws IOException {
     Logger.getLogger(MainApp.class.getName()).info(() -> "Hello word 2024.06.24!");
 
-    final double MAX_IAP = 166.0;
     final int TEMPO_BPM = 1200;
 
-    int iap = 171;
-    double[] dIn = {80, 90, 100, 135, 135, 135, 120, 115, 115, 140, 140, 140, 125, 120, 120, 145, 145, 145, 130, 125, 125, 150,
-        150, 150, 135, 130, 130, 155, 155, 155, 140, 135, 135, 160, 160, 160, 120, 100};
+    String name = "Kotov";
+    int iap = 150;
+    int minutes = 38;
+    double[] dIn = Arrays.copyOf(new double[] {80, 90, 100, 135, 135, 135, 120, 115, 115, 140, 140, 140, 125, 120, 120, 145, 145, 145, 130, 125, 125, 150,
+        150, 150, 135, 130, 130, 155, 155, 155, 140, 135, 135, 160, 160, 160, 120, 100}, minutes);
 
     double[] d = new double[dIn.length + 1];
     System.arraycopy(dIn, 0, d, 0, dIn.length);
@@ -37,7 +39,7 @@ public class MainApp {
     Pattern pattern = null;
 
     for (int i = 0; i < (d.length - 1) * TEMPO_BPM; ) {
-      double pulsePBM = Math.max(40, Math.min(interpolate.value(1.0 * i / TEMPO_BPM) * iap / MAX_IAP, 220));
+      double pulsePBM = Math.clamp(interpolate.value(1.0 * i / TEMPO_BPM) * iap / 166.0, 40, 220);
       int cadence = (int) Math.round(pulsePBM / 2.0);
       int len = TEMPO_BPM / cadence;
       i += len;
@@ -57,7 +59,7 @@ public class MainApp {
     }
 
     Logger.getLogger(MainApp.class.getName()).info(() -> "");
-    MidiFileManager.savePatternToMidi(pattern, new File("Training_%d.midi".formatted(iap)));
+    MidiFileManager.savePatternToMidi(pattern, new File("%s_%02d.midi".formatted(name, 1)));
     player.play(pattern);
   }
 }
