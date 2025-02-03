fda3d1fd8bae3fd887360172cf3b90ea65de676f
Alexander Kobelev <ak.mail.ru@gmail.com>
2025-02-03 20:34:38 +0300

    https://logback.qos.ch/manual/layouts.html

--- hello/src/main/java/com/ak/app/MainApp.java
+++ hello/src/main/java/com/ak/app/MainApp.java
@@ -1,14 +1,17 @@
 package com.ak.app;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.ZonedDateTime;
 
 public class MainApp {
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainApp.class);
+
   private MainApp() {
   }
 
   public static void main(String[] args) {
-    LoggerFactory.getLogger(MainApp.class).info("Hello World {}!", ZonedDateTime.now());
+    LOGGER.atInfo().setMessage(() -> "Hello World!").addKeyValue("time", ZonedDateTime::now).log();
   }
 }
